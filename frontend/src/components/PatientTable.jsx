6ee06c779db7fd2f9ea40a66cf2415ba26e732a1
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@Nguyens-Mac-mini.local>
2025-02-24 23:19:19 +0700

    ...

--- frontend/src/components/PatientTable.jsx
+++ frontend/src/components/PatientTable.jsx
@@ -1,20 +1,63 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Table } from 'antd'
+import { Table, Input, Space, Button } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
 import { setPatients as dispatchPatients } from '../slices/patient'
 import { getAllPatients } from '../api'
 
 const PatientTable = () => {
+	const [searchText, setSearchText] = useState('')
+	const [searchedColumn, setSearchedColumn] = useState('')
 	const { patients } = useSelector(state => state.patientSlice)
 	const rowSelection = {}
+	const dispatch = useDispatch()
+	const searchInput = useRef(null)
+
+	const handleSearch = (selectedKeys, confirm, dataIndex) => {
+		confirm()
+		setSearchText(selectedKeys[0])
+		setSearchedColumn(dataIndex)
+	}
+
+	const getColumnSearchProps = (dataIndex) => ({
+		filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+			<div
+				style={{ padding: 8 }}
+				onKeyDown={(e) => e.stopPropagation()}
+			>
+				<Input
+					ref={searchInput}
+					value={selectedKeys[0]}
+					onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+					onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+					style={{ marginBottom: 8, display: 'block' }}
+				/>
+			</div>
+		),
+		filterIcon: (filtered) => (
+			<SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+		),
+		onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+		filterDropdownProps: {
+			onOpenChange(open) {
+				if (open) {
+					setTimeout(() => searchInput.current.select(), 100)
+				}
+			}
+		}
+	})
+
 	const columns = [
 		{ key: 'id', title: 'ID', dataIndex: 'id' },
-		{ title: 'Họ', dataIndex: 'first_name' },
-		{ title: 'Tên', dataIndex: 'last_name' },
+		{ title: 'Họ', dataIndex: 'first_name', ...getColumnSearchProps('first_name') },
+		{
+			title: 'Tên', dataIndex: 'last_name',
+			...getColumnSearchProps('last_name')
+		},
 		{ title: 'Tuổi', dataIndex: 'age' },
 		{ title: 'Phone', dataIndex: 'phone' },
 	]
-	const dispatch = useDispatch()
+
 
 	useEffect(() => {
 		getAllPatients()
@@ -30,6 +73,8 @@ const PatientTable = () => {
 				dataSource={patients}
 				rowSelection={{ type: 'radio' }}
 				rowKey="id"
+				size="small"
+				scroll={{ y: 55 * 5 }}
 			/>
 		</div>
 	)
